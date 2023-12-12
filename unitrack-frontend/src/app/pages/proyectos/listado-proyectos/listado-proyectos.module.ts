dd29f4cc445c0c85f2820232ff4020fb0faa740b
Jose Villalta <josevillalta@vimasistem.com>
2023-12-12 10:35:42 -0500

    listado de proyectos

--- unitrack-frontend/src/app/pages/proyectos/listado-proyectos/listado-proyectos.module.ts
+++ unitrack-frontend/src/app/pages/proyectos/listado-proyectos/listado-proyectos.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ListadoProyectosRoutingModule } from './listado-proyectos-routing.module';
 import { ListadoProyectosComponent } from './listado-proyectos.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzDrawerModule, NzDrawerService } from 'ng-zorro-antd/drawer';
 
 @NgModule({
 	declarations: [ListadoProyectosComponent],
-	imports: [CommonModule, ListadoProyectosRoutingModule],
+	imports: [
+		CommonModule,
+		ListadoProyectosRoutingModule,
+		NzButtonModule,
+		NzInputModule,
+		NzTableModule,
+		NzIconModule,
+		NzToolTipModule,
+		NzDatePickerModule,
+		NzDrawerModule,
+	],
+	providers: [
+		NzDrawerService,
+	]
 })
 export class ListadoProyectosModule {}
